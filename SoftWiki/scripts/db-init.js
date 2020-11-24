4b61baf47e15106ca7f038c2662d3abb54924e5b
Ati <65487762+AtiVassileva@users.noreply.github.com>
2020-11-24 15:50:14 +0200

    Add files via upload

---
+++ SoftWiki/scripts/db-init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD9Of5dtoLRfMnGWdiTr3nhbUzJGZ7Y_Q0",
+    authDomain: "softwiki-6d4a3.firebaseapp.com",
+    databaseURL: "https://softwiki-6d4a3.firebaseio.com",
+    projectId: "softwiki-6d4a3",
+    storageBucket: "softwiki-6d4a3.appspot.com",
+    messagingSenderId: "308421837634",
+    appId: "1:308421837634:web:cf63ab42dab8de19d4fedb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
