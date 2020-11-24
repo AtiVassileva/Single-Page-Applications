4125dd335f5839f2b02e909d0aa28913fc270afa
Ati <65487762+AtiVassileva@users.noreply.github.com>
2020-11-24 11:28:42 +0200

    Add files via upload

---
+++ MyBlog/scripts/db-init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBFIrrujkTLnSHj-exHgF7phz1RZzwvCEo",
+    authDomain: "myblog-ac59a.firebaseapp.com",
+    databaseURL: "https://myblog-ac59a.firebaseio.com",
+    projectId: "myblog-ac59a",
+    storageBucket: "myblog-ac59a.appspot.com",
+    messagingSenderId: "740401855110",
+    appId: "1:740401855110:web:8b6d963c526a74fdfd6f07"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
