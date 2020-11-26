167b37bbaedf4dd15d6322e5a235eb5713306bf6
Ati <65487762+AtiVassileva@users.noreply.github.com>
2020-11-26 19:53:39 +0200

    Add files via upload

---
+++ SoftTerest/scripts/db-init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA4OClePg44zolPCeQAL_gIiLLP9rbPe28",
+    authDomain: "softterest-2fb90.firebaseapp.com",
+    databaseURL: "https://softterest-2fb90.firebaseio.com",
+    projectId: "softterest-2fb90",
+    storageBucket: "softterest-2fb90.appspot.com",
+    messagingSenderId: "256299648201",
+    appId: "1:256299648201:web:b950438387a633f2c7b918"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
