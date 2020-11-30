95aa8a506771c9d39d43f333620b60b2f4bf7019
Ati <65487762+AtiVassileva@users.noreply.github.com>
2020-11-30 20:38:26 +0200

    Add files via upload

---
+++ Spotify/scripts/db-init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCTsdF4-ZN0Avr5dyZWpAe4ubAm29Rn35I",
+    authDomain: "spotify-e7ad2.firebaseapp.com",
+    databaseURL: "https://spotify-e7ad2.firebaseio.com",
+    projectId: "spotify-e7ad2",
+    storageBucket: "spotify-e7ad2.appspot.com",
+    messagingSenderId: "56169491889",
+    appId: "1:56169491889:web:55bacb798d630f1b4b6190"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
